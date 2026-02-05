56ab6dcd755a161c1e463775d8afbeff6acccbba
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2026-02-05 16:56:44 +0545

    [ADDED]: - added multi step space form - added basic layout design

--- src/app/common/space-form/space-form.ts
+++ src/app/common/space-form/space-form.ts
@@ -1,11 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  Check,
+  Grid2x2,
+  Info,
+  LucideAngularModule,
+  LucideIconData,
+  Users,
+  Wifi,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-space-form',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './space-form.html',
   styleUrl: './space-form.css',
 })
 export class SpaceForm {
+  checkIcon = Check;
 
+  spaceForm:FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    space_type: new FormControl('',[Validators.required]),
+    space_dimension: new FormControl('', [Validators.required]),
+    space_description: new FormControl(''),
+    space_photos: new FormControl([], [Validators.minLength(1)])
+  })
+
+  multiStepList: WritableSignal<
+    Array<{
+      id: number;
+      name: string;
+      active: boolean;
+      completed: boolean;
+      icon: LucideIconData;
+    }>
+  > = signal([
+    {
+      id: 1,
+      name: 'General Information',
+      active: true,
+      completed: false,
+      icon: Info,
+    },
+    {
+      id: 2,
+      name: 'Capacity and Layout',
+      active: false,
+      completed: false,
+      icon: Users,
+    },
+    {
+      id: 3,
+      name: 'Amenities',
+      active: false,
+      completed: false,
+      icon: Wifi,
+    },
+    {
+      id: 4,
+      name: 'Photo Upload',
+      active: false,
+      completed: false,
+      icon: Grid2x2,
+    },
+  ]);
+
+  currentActiveStepId = computed(() => {
+    return this.multiStepList().find(steps => steps.active)!.id
+  })
+
+  changeActiveStep(id: number) {
+    this.multiStepList.update((steps) =>
+      steps.map((step) =>
+        step.id == id
+          ? {
+              ...step,
+              active: true,
+            }
+          : {
+            ...step,
+            active: false
+          },
+      ),
+    );
+  }
 }
